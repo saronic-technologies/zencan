10474a9d3ac5699747d34c546993667c065388cd
Jeff McBride <jeff@jeffmcbride.net>
2025-05-07 13:00:29 -0700

    Better error message on integration test device config parsing error

--- integration_tests/build.rs
+++ integration_tests/build.rs
@@ -1,5 +1,11 @@
 
 fn main() {
-    zencan_build::build_node_from_device_config("EXAMPLE1", "device_configs/example1.toml").unwrap();
-    zencan_build::build_node_from_device_config("EXAMPLE2", "device_configs/example2.toml").unwrap();
+    if let Err(e) = zencan_build::build_node_from_device_config("EXAMPLE1", "device_configs/example1.toml") {
+        eprintln!("Error building node from example1.toml: {}", e);
+        std::process::exit(1);
+    }
+    if let Err(e) = zencan_build::build_node_from_device_config("EXAMPLE2", "device_configs/example2.toml") {
+        eprintln!("Error building node from example2.toml: {}", e);
+        std::process::exit(1);
+    }
 }
