d0a2925f6814d5e4dab9ad8c17788e011e9f585d
Jeff McBride <jeff@jeffmcbride.net>
2025-05-10 14:42:32 -0700

    zencandump uses socketcan wrappers

--- zencan-cli/src/zencandump.rs
+++ zencan-cli/src/zencandump.rs
@@ -1,48 +1,33 @@
-use socketcan::{Frame, EmbeddedFrame};
-use zencan_client::common::{
-    messages::{CanError, CanId, CanMessage, ZencanMessage},
-    traits::AsyncCanReceiver,
-};
 use clap::Parser;
+use zencan_client::common::{
+    messages::{MessageError, ZencanMessage},
+    traits::AsyncCanReceiver,
+    CanMessage,
+};
 
 #[derive(Parser)]
 struct Args {
     socket: String,
+    #[clap(short, long)]
+    verbose: bool,
 }
 
 pub enum Message {
-    Unrecognized(CanMessage),
+    Unrecognized {
+        msg: CanMessage,
+        reason: MessageError,
+    },
     Recognized(ZencanMessage),
 }
 
-/// Convert a socketcan CanId to a zencan CanId
-fn convert_rx_canid(rx_id: socketcan::CanId) -> CanId {
-    match rx_id {
-        socketcan::CanId::Standard(standard_id) => CanId::std(standard_id.as_raw()),
-        socketcan::CanId::Extended(extended_id) => CanId::extended(extended_id.as_raw()),
-    }
-}
-
-impl TryFrom<socketcan::CanFrame> for Message {
-    type Error = CanError;
-    fn try_from(value: socketcan::CanFrame) -> Result<Self, Self::Error> {
-        let id = convert_rx_canid(value.can_id());
-
-        // Convert to zencan CanMessage, unless it is an error frame
-        let msg = match value {
-            socketcan::CanFrame::Data(frame) => CanMessage::new(id, frame.data()),
-            socketcan::CanFrame::Remote(_) => CanMessage::new_rtr(id),
-            socketcan::CanFrame::Error(frame) =>
-                return Err(CanError::from_raw(frame.error_bits() as u8)),
-        };
-
+impl From<CanMessage> for Message {
+    fn from(msg: CanMessage) -> Self {
         // Attempt to parse as a recognized Zencan message, and fallback to displaying it as a
         // generic can message
-        let zenmsg: Result<ZencanMessage, _> = msg.try_into();
-        Ok(match zenmsg {
+        match msg.try_into() {
             Ok(msg) => Message::Recognized(msg),
-            Err(_) => Message::Unrecognized(msg),
-        })
+            Err(e) => Message::Unrecognized { msg, reason: e },
+        }
     }
 }
 
@@ -54,8 +39,16 @@ async fn main() {
     loop {
         if let Ok(msg) = rx.recv().await {
             let time = chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Micros, false);
-            println!("{time}: {msg:?}");
+
+            match msg.into() {
+                Message::Recognized(msg) => println!("{time}: {msg:?}"),
+                Message::Unrecognized { msg, reason } => {
+                    println!("{time}: {msg:?}");
+                    if args.verbose {
+                        println!("Unrecognized reason: {reason:?}");
+                    }
+                }
+            }
         }
     }
-
 }
